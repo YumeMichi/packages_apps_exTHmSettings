d7ab456398bffce8a02889a1d996d984c639313e
DennySPb <dennyspb@gmail.com>
2021-09-07 14:24:40 +0300

    exTHmUI: GamingMode: Opt-out performance profile support [3/3]
    Change-Id: Icdc2cd10afe6735c9eed478dedeb186c56530006

--- src/org/exthmui/settings/fragments/GamingModeSettings.java
+++ src/org/exthmui/settings/fragments/GamingModeSettings.java
@@ -22,6 +22,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.provider.Settings;
 
+import androidx.preference.PreferenceCategory;
 import androidx.preference.SwitchPreference;
 import androidx.preference.PreferenceScreen;
 
@@ -36,6 +37,7 @@ import lineageos.hardware.LineageHardwareManager;
 
 public class GamingModeSettings extends SettingsPreferenceFragment {
 
+    private boolean mPerformanceSupported;
     private SwitchPreference mHardwareKeysDisable;
     private PackageListPreference mGamingPrefList;
 
@@ -55,6 +57,16 @@ public class GamingModeSettings extends SettingsPreferenceFragment {
 
         mGamingPrefList = (PackageListPreference) findPreference("gaming_mode_app_list");
         mGamingPrefList.setRemovedListKey(Settings.System.GAMING_MODE_REMOVED_APP_LIST);
+
+        final PreferenceCategory perfCat = (PreferenceCategory) prefScreen
+                .findPreference("performance_category");
+
+        mPerformanceSupported = getResources().getBoolean(
+                    com.android.internal.R.bool.config_gamingmode_performance);
+
+        if (!mPerformanceSupported) {
+            prefScreen.removePreference(perfCat);
+        }
     }
 
     @Override
